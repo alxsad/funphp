031ab2c7fac49228a6b54472d509f3a983a9d68c
Alex Davidovich <alexander.davidovich@intellectsoft.net>
2014-01-26 16:31:10 +0300

    new functions: any, all

--- src/funphp.php
+++ src/funphp.php
@@ -3,21 +3,39 @@
 namespace funphp;
 
 function each ($c, \Closure $fn) {
-	foreach ($c as $k => $v) {
-		$fn($v, $k, $c);
-	}
+    foreach ($c as $k => $v) {
+        $fn($v, $k, $c);
+    }
 }
 
 function map ($c, \Closure $fn) {
-	foreach ($c as $k => $v) {
-		yield $k => $fn($v, $k, $c);
-	}
+    foreach ($c as $k => $v) {
+        yield $k => $fn($v, $k, $c);
+    }
 }
 
 function filter ($c, \Closure $fn) {
-	foreach ($c as $k => $v) {
-		if ($fn($v, $k)) {
-			yield $k => $v;
-		}
-	}
+    foreach ($c as $k => $v) {
+        if ($fn($v, $k, $c)) {
+            yield $k => $v;
+        }
+    }
+}
+
+function any ($c, \Closure $fn) {
+    foreach ($c as $k => $v) {
+        if ($fn($v, $k, $c)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function all ($c, \Closure $fn) {
+    foreach ($c as $k => $v) {
+        if (!$fn($v, $k, $c)) {
+            return false;
+        }
+    }
+    return true;
 }
--- test/success.php
+++ test/success.php
@@ -10,31 +10,61 @@ class Success extends \PHPUnit_Framework_TestCase
         $output = [];
 
         f\each($input, function ($v) use (&$output) {
-        	$output[] = $v + 1;
+            $output[] = $v + 1;
         });
 
-		$this->assertEquals([2, 3, 4], $output);
+        $this->assertEquals([2, 3, 4], $output);
     }
 
     public function testMap()
     {
-    	$input = [1, 2, 3];
+        $input = [1, 2, 3];
 
-    	$output = iterator_to_array(f\map($input, function ($v) {
-    		return $v * 2;
-    	}));
+        $output = iterator_to_array(f\map($input, function ($v) {
+            return $v * 2;
+        }));
 
-    	$this->assertEquals([2, 4, 6], $output);
+        $this->assertEquals([2, 4, 6], $output);
     }
 
     public function testFilter()
     {
-    	$input = [1, 2, 3, 4, 5];
+        $input = [1, 2, 3, 4, 5];
 
-    	$output = iterator_to_array(f\filter($input, function ($v) {
-    		return $v & 1;
-    	}));
-    	
-    	$this->assertEquals([1, 3, 5], array_values($output));
+        $output = iterator_to_array(f\filter($input, function ($v) {
+            return $v & 1;
+        }));
+
+        $this->assertEquals([1, 3, 5], array_values($output));
+    }
+
+    public function testAny()
+    {
+        $input = [1, 2, 3, 4, 5];
+
+        $result = f\any($input, function ($v) {
+            return $v == 3;
+        });
+        $this->assertEquals(true, $result);
+
+        $result = f\any($input, function ($v) {
+            return $v == 6;
+        });
+        $this->assertEquals(false, $result);
+    }
+
+    public function testAll()
+    {
+        $input = [1, 2, 3, 4, 5];
+        $result = f\all($input, function ($v) {
+            return is_numeric($v);
+        });
+        $this->assertEquals(true, $result);
+
+        $input = [1, 2, 3, 4, 'test'];
+        $result = f\all($input, function ($v) {
+            return is_numeric($v);
+        });
+        $this->assertEquals(false, $result);
     }
 }
